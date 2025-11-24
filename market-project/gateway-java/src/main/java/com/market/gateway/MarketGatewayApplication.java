cc2ecbc0bf41a59b96b45da0ecd5ccbc6bf4eb8b
Cristian <cristian.ciulica@student.unitbv.ro>
2025-11-24 11:26:27 +0200

    GeminiAPI TODO

--- market-project/gateway-java/src/main/java/com/market/gateway/MarketGatewayApplication.java
+++ market-project/gateway-java/src/main/java/com/market/gateway/MarketGatewayApplication.java
@@ -1,8 +1,7 @@
 package com.market.gateway;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// Implement Gemini API in this branch.
 @SpringBootApplication
 public class MarketGatewayApplication {
 
